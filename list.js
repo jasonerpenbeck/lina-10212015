ac280e3330db9feda2bb8a67d9ee8228c43f2be9
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2015-10-21 09:51:28 -0700

    HTML and JS file for type ahead test

---
+++ list.js
@@ -0,0 +1,12 @@
+
+
+var list = [
+  {name: 'Jason', id: 1, position: 'Full Stack Engineer'},
+  {name: 'Jason', id: 2, position: 'Back End Engineer'},
+  {name: 'Kia', id: 3, position: 'iOS Engineer'},
+  {name: 'Lin', id: 4, position: 'Designer'},
+  {name: 'Andi', id: 5, position: 'Director of Community'},
+  {name: 'Eric', id: 6, position: 'Co-Founder & CEO'},
+  {name: 'Tony', id: 7, position: 'Co-Founder'},
+  {name: 'Tanya', id: 8, position: 'VP, Product'},
+];
